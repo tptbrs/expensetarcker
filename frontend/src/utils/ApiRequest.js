bbb2fcf4a59213847610ffe8d847566dfbfc5399
maybesushantt <tptbrs16@gmail.com>
2025-04-05 13:56:32 +0530

    Updated API URLs to connect frontend and backend

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,5 +1,8 @@
-const host = "http://localhost:5000";
+const host = "https://expensetaracker.vercel.app";
 // const host = "http://localhost:5000";
+// const API = process.env.REACT_APP_API_URL;
+
+// axios.get(`${API}/api/your-route`);
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
 export const loginAPI = `${host}/api/auth/login`;
